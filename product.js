1180f94564c10d86e0f739570a6af8457f1df356
harikrishna <harikrishna6525@gmail.com>
2022-06-26 13:56:57 +0530

    create a all products

--- product.js
+++ product.js
@@ -1,1 +1,41 @@
-const url = 'https://course-api.com/javascript-store-products';
+// const url = 'https://course-api.com/javascript-store-products';
+const url = 'https://course-api.com/react-store-products';
+// desination for the products in the store
+const createproducts = document.querySelector('.product_row');
+// get the product in json 
+const fetchProduct =  async () => {
+    const product = await fetch(url);
+    const response = await product.json();
+    return response;
+}
+
+
+
+const displayProduct =  (list) => {
+    console.log('displayProduct', list);
+  const productList = list.map(function (product){
+     const {id}= product;
+     const{name:title,price}=product;     
+     const{image}= product;  
+
+     const formatPrice = price/100;
+     //id,image,price,title
+    return `<div class="product_card">
+    <img src="${image}"
+        alt="no product-img">
+    <p class="Product_name">${title}</p>
+    <P>${formatPrice}</P>
+    <a>add Cart</a>
+</div>`
+
+   }).join("");
+   
+   createproducts.innerHTML=productList;
+   console.log(createproducts);
+}
+const getProduct=async ()=>  {
+    const data = await fetchProduct();
+    displayProduct(data);
+    
+}
+getProduct();
